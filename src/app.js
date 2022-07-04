8a5d6d07c1ce2e7c032df5b2969bf245553778e0
Darya <daryapleshko.work.dev@mail.ru>
2022-07-04 19:58:03 +0300

    changed js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const btn = document.querySelector('.create-account');
 
 
 const check = (name, email, password, confirmPassword) => {
-    if (name.length == 0 || email.length == 0 || password.length == 0 || confirmPassword.length == 0) throw new Error('Вы не ввели всю информацию !');
+    if (name.length === 0 || email.length == 0 || password.length == 0 || confirmPassword.length == 0) throw new Error('Вы не ввели всю информацию !');
     if (!/^[а-яА-Яa-zA-Z ]+$/g.test(name)) throw new Error('Некорректно введено ФИО !');
     if (!/^[a-z0-9_.]+@[a-z0-9_.]+\.[a-z]+$/g.test(email)) throw new Error('Некорректно введенный email !');
-    if (password.length < 7) throw new Error('Пароль должен содержать минимум 8 символов !');
+    if (password.length <= 7) throw new Error('Пароль должен содержать минимум 8 символов !');
     if (password.split('').sort().join('') != confirmPassword.split('').sort().join('')) throw new Error('Пароли не совпадают !');
     return true;
 }
@@ -14,14 +14,13 @@ const check = (name, email, password, confirmPassword) => {
 btn.addEventListener('click', () => {
     try {
 
-        const name = document.querySelector('.name').value;
-        const email = document.querySelector('.email').value;
-        const password = document.querySelector('.password').value;
-        const confirmPassword = document.querySelector('.confirm-password').value;
+        const name = document.querySelector('.name').value.trim();
+        const email = document.querySelector('.email').value.trim();
+        const password = document.querySelector('.password').value.trim();
+        const confirmPassword = document.querySelector('.confirm-password').value.trim();
 
-        if (check(name, email, password, confirmPassword)) {
-            alert('Вы успешно зарегистрированы в системе');
-        }
+        if (check(name, email, password, confirmPassword)) alert('Вы успешно зарегистрированы в системе');
+
     } catch (error) {
         alert(error.message);
     }
