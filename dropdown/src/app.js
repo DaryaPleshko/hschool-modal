28295fb9d264caba865e5050ed4fa28410c93169
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-13 22:27:38 +0300

    commit

--- dropdown/src/app.js
+++ dropdown/src/app.js
@@ -1,9 +1,7 @@
-const result = document.querySelector('.result');
-const inp = document.querySelector('.inp');
-const img = document.querySelector('.img');
+const result = document.querySelector('.result'),
+    inp = document.querySelector('.inp'),
+    img = document.querySelector('.img');
 
-
- 
 const upArrow = `position: absolute;
 background-image: url('C:/Users/Hanna/Desktop/hs/dropdown/img/Chevron2.jpg');
 background-repeat: no-repeat;
@@ -12,7 +10,7 @@ height: 15px;
 top: 22px;
 right: 11px;
 `;
-const downArrow =`position: absolute;
+const downArrow = `position: absolute;
 background-image: url('C:/Users/Hanna/Desktop/hs/dropdown/img/Chevron.jpg');
 background-repeat: no-repeat;
 width: 15px;
@@ -21,21 +19,15 @@ top: 22px;
 right: 11px;
 `;
 
-
 const displaySelection = () => {
     return (!document.querySelector('.result p')) ? true : false;
 }
 
-
 inp.addEventListener('click', () => {
     try {
-
         img.style = `${upArrow}`;
-
         if (displaySelection()) {
-
             const array = ['Businessman', 'Employee', 'Freelancer', 'Retired'];
-
             for (let i = 0; i < array.length; i++) {
                 const p = document.createElement("p");
                 p.className = `class-p${i}`;
@@ -43,12 +35,10 @@ inp.addEventListener('click', () => {
                 result.appendChild(p);
                 document.querySelector(`.class-p${i}`).innerHTML = array[i];
             }
-
         } else {
             result.innerHTML = '';
             img.style = `${downArrow}`;
         }
-
     } catch (error) {
         alert(error.message);
     }
